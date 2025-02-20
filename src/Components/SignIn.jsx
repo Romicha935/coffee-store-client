879ca1126ec005f2d62852e43ba27ff8c2e5d9f9
romicha-parvin <romichaparvin35@gmail.com>
2025-02-20 19:32:22 +0600

    coffe store

---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../Providers/AuthProvider'
+
+const SignIn = () => {
+  const { SignInuser} = useContext(AuthContext)
+  const handleSignIn = e => {
+    e.preventDefault()
+    const form = e.target ;
+    const email = form.email.value;
+    const password = form.password.value
+    console.log(email,password);
+
+    SignInuser(email,password)
+    .then(result=>{
+      console.log(result.user);
+      const user = {
+        email,
+        lastLoggedAt: result.user?.metadata?.lastSignInTime
+
+      }
+      //update last logged at in the database
+      fetch('http://localhost:5000/user',{
+        method: 'PATCH',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then(res=> res.json())
+      .then(data=> {
+        console.log(data);
+        
+      })
+    })
+    .catch(error=>{
+       console.error(error);
+       
+    })
+    
+  }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="text-center lg:text-left">
+        <h1 className="text-5xl font-bold">Login now!</h1>
+        <p className="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+      </div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form onSubmit={handleSignIn} className="card-body">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">Email</label>
+            <input type="email" name='email' className="input" placeholder="Email" />
+            <label className="fieldset-label">Password</label>
+            <input type="password"
+            name='password' className="input" placeholder="Password" />
+            <div><a className="link link-hover">Forgot password?</a></div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default SignIn
